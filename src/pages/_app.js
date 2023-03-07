decf639c48f59b56cc6db2d570ded36f06d564cf
George <gwmkwan@gmail.com>
2023-03-06 21:04:36 -0700

    chore: add notes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,14 +3,22 @@ import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 
+// Set up chain and provider
 const { chains, provider } = configureChains(
   [chain.mainnet],
   [publicProvider()]
 );
 
+// Set up wallet
 const { connectors } = getDefaultWallets({
   appName: 'RainbowKit Demo',
   chains,
+});
+
+// Create client
+const wagmiClient = createClient({
+  connectors,
+  provider,
 });
 
 export default function App({ Component, pageProps }) {
