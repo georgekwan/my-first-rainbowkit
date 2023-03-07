f57ca3e217d466466faaa832ceb07bd4327da140
George <gwmkwan@gmail.com>
2023-03-06 20:56:29 -0700

    feat: import rainbowkit and peers

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
